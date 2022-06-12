0630fd0e85f4472e613f7fe88e759dd71797cac5
samjaing <53161715+samjaing@users.noreply.github.com>
2022-06-12 20:51:11 +1000

    Add files via upload
    Kafka api and solution file added.

---
+++ KafkaApi/Controllers/ProducerController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Confluent.Kafka;
+
+namespace KafkaApi.Controllers
+{
+    [ApiController]
+    [Route("Producer")]
+    public class ProducerController : ControllerBase
+    {
+        private readonly ProducerConfig config = new ProducerConfig
+        { BootstrapServers = "localhost:9092" };
+        private readonly string topic = "simpletalk_topic";
+
+        //./kafka-topics.sh --create --bootstrap-server kafka-broker-1:9092 --replication-factor 1 --partitions 1 --topic simpletalk_topic
+
+        //./kafka-topics.sh --create --bootstrap-server kafka-broker-1:9092 --replication-factor 1 --partitions 1 --topic simpletalk_topic
+
+
+        [HttpPost]
+        public IActionResult Post([FromQuery] string message)
+        {
+            return Created(string.Empty, SendToKafkaAsync(topic, message));
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            Console.WriteLine($"sambhav");
+            return Created(string.Empty, SendToKafkaAsync(topic, "hoha").Result);
+        }
+
+        private async Task<object> SendToKafkaAsync(string topic, string message)
+        {
+            Action<DeliveryReport<Null, string>> handler = r =>
+           Console.WriteLine(!r.Error.IsError
+               ? $"Delivered message to {r.TopicPartitionOffset}"
+               : $"Delivery Error: {r.Error.Reason}");
+
+            using (var producer =
+                 new ProducerBuilder<Null, string>(config).Build())
+            {
+                try
+                {
+                    //producer.Produce(topic, new Message<Null, string> { Value = message }, handler);
+                    var dr = await producer.ProduceAsync(topic, new Message<Null, string> { Value = message });
+                    Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset}'");
+                    return "success";
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Oops, something went wrong: {e}");
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ KafkaApi/Models/Consumer.cs
@@ -0,0 +1,42 @@
+﻿using Confluent.Kafka;
+
+namespace KafkaApi.Models
+{
+    public class Consumer : IHostedService
+    {
+        private readonly string topic = "simpletalk_topic";
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            var conf = new ConsumerConfig
+            {
+                GroupId = "st_consumer_group",
+                BootstrapServers = "localhost:9092",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+            using (var builder = new ConsumerBuilder<Ignore,
+                string>(conf).Build())
+            {
+                builder.Subscribe(topic);
+                var cancelToken = new CancellationTokenSource();
+                try
+                {
+                    while (true)
+                    {
+                        var consumer = builder.Consume(cancelToken.Token);
+                        Console.WriteLine($"Message: {consumer.Message.Value} received from {consumer.TopicPartitionOffset}");
+                    }
+                }
+                catch (Exception)
+                {
+                    builder.Close();
+                }
+            }
+            return Task.CompletedTask;
+        }
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+        
+    }
+}
